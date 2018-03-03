7cff7c4fb5a93a3aff6864e110f66e08272acd60
Félix <the2deux@gmail.com>
2018-03-03 04:13:16 -0500

    Added validation on forms

--- themes/airevisuelle/assets/js/main.js
+++ themes/airevisuelle/assets/js/main.js
@@ -1,8 +1,10 @@
 //@prepros-prepend jquery.js
 //@prepros-prepend functions.js
 //@prepros-prepend plugins.js
+//@prepros-prepend jquery.validate.min.js
 
 //custom JS goes here!
 $(document).ready(function(){
 		$('.col-eq-height').matchHeight();
+		$('form.widget-contact-form').validate();
 });
